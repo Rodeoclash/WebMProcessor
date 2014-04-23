6e479eebd7706c4f79bba480d655a21f947217ef
Samuel Richardson <sam@richardson.co.nz>
2014-04-24 07:53:47 +1000

    Force looking outside of current scope for name.

--- config/initializers/firebase_progress.rb
+++ config/initializers/firebase_progress.rb
@@ -1,24 +1,21 @@
-class FirebaseProgress
+	class FirebaseProgress
 
-	def initialize(options)
-		@uuid = options[:uuid]
-		@client = Firebase::Client.new(ENV['FIREBASE_URI'])
+		def initialize(options)
+			@uuid = options[:uuid]
+			@client = ::Firebase::Client.new(ENV['FIREBASE_URI'])
+		end
+
+		def update(details)
+			@client.set(@uuid, {
+				:progress_percentage => details[:progress_percentage],
+				:status => details[:status],
+				:ready_for_download => details[:ready_for_download],
+				:error_with_transcoding => details[:error_with_transcoding]
+			})
+		end
+
+		def remove
+			@client.delete(@uuid)
+		end
+
 	end
-
-	def update(details)
-		#Rails.logger.debug "Status: #{details[:status]}, Progress: #{details[:progress_percentage]}, Download ready: #{details[:ready_for_download]}, Had error: #{details[:error_with_transcoding]}"
-		#puts "Status: #{details[:status]}, Progress: #{details[:progress_percentage]}"
-		@client.set(@uuid, {
-			:progress_percentage => details[:progress_percentage],
-			:status => details[:status],
-			:ready_for_download => details[:ready_for_download],
-			:error_with_transcoding => details[:error_with_transcoding]
-		})
-	end
-
-	# removes the data from the firebase endpoint
-	def remove
-		@client.delete(@uuid)
-	end
-
-end
