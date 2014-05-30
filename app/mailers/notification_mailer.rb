2a655a564a2b02cd502ad22feaea5c60c6e63bc1
Samuel Richardson <sam@richardson.co.nz>
2014-05-30 21:51:12 +1000

    Better error checking

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,14 +4,14 @@ class NotificationMailer < ActionMailer::Base
 
   def starting_transcode(encoding_job_id)
     @encoding_job = EncodingJob.find(encoding_job_id)
-    if @encoding_job.notification_email_address
+    unless @encoding_job.notification_email_address.blank?
       mail(:to => @encoding_job.notification_email_address, :subject => "Transcode Starting")
     end
   end
 
   def finished_transcode(encoding_job_id)
     @encoding_job = EncodingJob.find(encoding_job_id)
-    if @encoding_job.notification_email_address
+    unless @encoding_job.notification_email_address.blank?
       mail(:to => @encoding_job.notification_email_address, :subject => "Transcode Finished")
     end
   end
